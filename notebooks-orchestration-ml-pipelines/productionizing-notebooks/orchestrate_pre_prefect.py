f9dac8dd660bf0872f007f471af689411ac2e777
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-06-25 16:03:42 +0300

    Feat(add): added traditional orchestration

---
+++ notebooks-orchestration-ml-pipelines/productionizing-notebooks/orchestrate_pre_prefect.py
@@ -0,0 +1,101 @@
+import pathlib
+import pickle
+import pandas as pd
+import numpy as np
+import scipy
+import sklearn
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import mean_squared_error
+import mlflow
+import xgboost as xgb
+from prefect import flow, task
+
+def read_data(filename: str) -> pd.DataFrame:
+    """Read data from filename in to a Dataframe"""
+    df = pd.read_parquet(filename)
+
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+
+    return df
+
+def add_features(
+        df_train: pd.DataFrame, df_val: pd.DataFrame
+) -> tuple(
+    [
+        scipy.sparse._csr.csr_matrix,
+        scipy.sparse._csr.csr_matrix,
+        np.ndarray,
+        np.ndarray,
+        sklearn.feature_extraction.DictVectorizer,
+    ]
+):
+    """Add features to the model"""
+    df_train["PU_DO"] = df_train["PULocationID"] + "_" + df_train["DOLocationID"]
+    df_val["PU_DO"] = df_val["PULocationID"] + "_" + df_val["DOLocationID"]
+
+    categorical = ["PU_DO"]
+    numerical = ["trip_distance"]
+
+    dv = DictVectorizer()
+
+    train_dicts = df_train[categorical + numerical].to_dict(orient="records")
+    X_train = dv.fit_transform(train_dicts)
+
+    val_dicts = df_val[categorical + numerical].to_dict(orient="records")
+    X_val = dv.transform(val_dicts)
+
+    y_train = df_train["duration"].values
+    y_val = df_val["duration"].values
+
+    return X_train, X_val, y_train, y_val, dv
+
+def train_best_model(
+        X_train: scipy.sparse._csr.csr_matrix,
+        X_val: scipy.sparse._csr.csr_matrix,
+        y_train: np.ndarray,
+        y_val: np.ndarray,
+        dv: sklearn.feature_extraction.DictVectorizer,
+) -> None:
+    """Train a model with best hyper parameters and write everything out"""
+    with mlflow.start_run():
+        train = xgb.DMatrix(X_train, label=y_train)
+        valid = xgb.DMatrix(X_val, label=y_val)
+
+        best_params = {
+            "learning_rate": 0.09585355369315604,
+            "max_depth": 30,
+            "min_child_weight": 1.060597050922164,
+            "objective": "reg:linear",
+            "reg_alpha": 0.018060244040060163,
+            "reg_lambda": 0.011658731377413597,
+            "seed": 42,
+        }
+
+        mlflow.log_params(best_params)
+
+        booster = xgb.train(
+            params=best_params,
+            dtrain=train,
+            num_boost_round=100,
+            evals=[(valid, "validation")],
+            early_stopping_rounds=20,
+        )
+
+        y_pred = booster.predict(valid)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
+
+        pathlib.Path("models").mkdir(exist_ok=True)
+        with open("models/preprocessor.b", "wb") as f_out:
+            pickle.dump(dv, f_out)
+        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+    return None
+
+    
