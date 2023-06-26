cc1b78b94560e52a9b72796c1184d9bcec5a5465
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-06-26 02:09:33 -0700

    Feat(Add): removed ssl verification

--- notebooks-orchestration-ml-pipelines/prefect-mlops-101/cat_facts.py
+++ notebooks-orchestration-ml-pipelines/prefect-mlops-101/cat_facts.py
@@ -4,7 +4,7 @@ from prefect import task, flow
 @task(retries=4, retry_delay_seconds=0.1, log_prints=True)
 def fetch_cat_facts():
     dummy_cat_url = "https://f3-vyx5c2hfpq-ue.a.run.app/"
-    cat_fact = httpx.get(dummy_cat_url)
+    cat_fact = httpx.get(dummy_cat_url, verify=False)
     #An endpoint that is designed to fail sporadically
     if cat_fact.status_code >= 400:
         raise Exception
--- notebooks-orchestration-ml-pipelines/prefect-mlops-101/dad_jokes.py
+++ notebooks-orchestration-ml-pipelines/prefect-mlops-101/dad_jokes.py
@@ -5,7 +5,7 @@ from prefect import flow, task
 @task(retries=4, retry_delay_seconds=0.1, log_prints=True)
 def pull_dad_jokes():
     api_url = "https://api.api-ninjas.com/v1/dadjokes?limit="
-    response = httpx.get("https://api.api-ninjas.com/v1/dadjokes?limit=2", headers={'X-Api-Key': config.api_key}).json()[0]
+    response = httpx.get("https://api.api-ninjas.com/v1/dadjokes?limit=2", headers={'X-Api-Key': config.api_key},verify=False).json()[0]
 
     print(response["joke"])
 
