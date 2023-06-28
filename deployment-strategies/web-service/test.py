79ce31197c29d698aa565bf3ca0c4272743d4fea
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-06-28 17:33:42 +0300

    Feat(add): added deployment strategy webservice files

---
+++ deployment-strategies/web-service/test.py
@@ -0,0 +1,16 @@
+import requests
+# This is delibrately added since I have shifted from scikit learn 1.2.1 to 1.2.2 since deployments started
+import warnings
+warnings.filterwarnings("ignore")
+
+
+ride = {
+    "PULocationID": 10, 
+    "DOLocationID": 50, 
+    "trip_distance": 5
+}
+
+url = 'http://127.0.0.1:9696/predict'
+
+response = requests.post(url, json=ride)
+print(response.json())
